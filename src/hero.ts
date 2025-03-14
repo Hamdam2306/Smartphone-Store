93ecc367d9c60dd541b38505585586adf7bc151e
Hamdam Hurmatullayev <hurmatullayevhamdambek@gmail.com>
2025-03-15 00:51:05 +0500

    hero statrted

--- src/hero.ts
+++ src/hero.ts
@@ -1,1 +1,16 @@
-console.log('Hello from hero.ts');
+import { body } from "./elements";
+
+console.log("Hello from hero.ts");
+
+// const title = document.createElement("h1");
+// const card = document.createElement('div')
+// const  = document.createElement('')
+
+window.addEventListener('load', () => {
+   const title = document.createElement('h1')
+   title.innerText = 'our products'
+   body?.appendChild(title)
+})
+
+
+
